2a17aa46f7fd258271523ca822f30648027f1706
reamaejocel <rmjcledera07@gmail.com>
2022-12-30 14:54:51 +0800

    added code in style.css

---
+++ index.js
@@ -0,0 +1,48 @@
+let weather = {
+    apiKey: "27f3e7be748fb5d6a9caf13a09df6657",
+    fetchWeather: function (city) {
+        fetch (
+            "http://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid=" 
+            + this.apiKey
+        ) 
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+   displayWeather: function(data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name,icon,description,temp,humidity,speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage = "url('https://media.istockphoto.com/id/1092423872/photo/skyline-of-manila-city-and-manila-bay-philippines.jpg?b=1&s=612x612&w=0&k=20&c=Y2-TTRn7QusGhRGIPz5_B3_wJrN0wLkYQwCxeRDF5X4="  + name + "')"
+
+   },
+   search: function() {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+   }
+
+};
+
+document
+.querySelector(".search button")
+.addEventListener("click", function() {
+    weather.search();
+
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})
+
+weather.fetchWeather("The Philippines");
